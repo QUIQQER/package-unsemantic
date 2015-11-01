a9eca30d435702712911de7bd758ea917d9fb54a
Henning Leutz <leutz@pcsg.de>
2015-11-01 10:13:47 +0100

    Smarty unsemantic function {unsemantic}

---
+++ src/QUI/Unsemantic/EventHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file contains QUI\Unsemantic\EventHandler
+ */
+namespace QUI\Unsemantic;
+
+use QUI;
+
+/**
+ * Class EventHandler
+ * @package QUI\Unsemantic
+ */
+class EventHandler
+{
+
+    /**
+     * Event : on smarty init
+     * @param \Smarty $Smarty - \Smarty
+     */
+    static function onSmartyInit($Smarty)
+    {
+        // {unsemantic}
+        if (!isset($Smarty->registered_plugins['function']) ||
+            !isset($Smarty->registered_plugins['function']['unsemantic'])
+        ) {
+            $Smarty->registerPlugin("function", "unsemantic", "\QUI\Unsemantic\EventHandler::unsemantic");
+        }
+    }
+
+    /**
+     * Smarty brickarea function {brickarea}
+     *
+     * @param Array $params - function parameter
+     * @param \Smarty $smarty - \Smarty
+     * @return Array|String
+     */
+    static function unsemantic($params, $smarty)
+    {
+        return '<link href="'. URL_OPT_DIR .'bin/unsemantic/assets/stylesheets/unsemantic-grid-responsive.css"
+          rel="stylesheet"
+          type="text/css"
+               />';
+    }
+}
